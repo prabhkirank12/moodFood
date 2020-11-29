84a6bb58186503b43eaa95a1a85c256e5029bdfe
prabhkirank12 <prabhkirank2@gmail.com>
2020-11-29 14:48:38 -0500

    Set up for moodFood

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const db = require('./config/keys').mongoURI;
+const port = process.env.PORT || 5000;
+const mongoose = require('mongoose');
+const users = require("./routes/api/users");
+const bodyParser = require('body-parser');
+
+mongoose
+    .connect(db, { useNewUrlParser: true })
+    .then(() => console.log("Connected to MongoDB successfully"))
+    .catch(err => console.log(err));
+
+app.get("/", (req, res) => res.send("Welcome to moodFood"));
+app.use("/api/users", users);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.listen(port, () => console.log(`Server is running on port ${port}`));
