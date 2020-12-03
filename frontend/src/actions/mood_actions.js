eda4a38ca49f1500eedfd3f53963a98c7c5f2e1b
littlecourage <christine.adams21@gmail.com>
2020-12-03 14:46:40 -0500

    Added moodsExist to user's state, in action, reducer, and util

--- frontend/src/actions/mood_actions.js
+++ frontend/src/actions/mood_actions.js
@@ -1,16 +1,29 @@
 import * as APIUtil from '../util/mood_api_util';
 
 export const RECEIVE_USER_MOOD = "RECEIVE_USER_MOOD";
+export const RECEIVE_MOODS_EXIST = "RECEIVE_MOODS_EXIST";
 
 const receiveUserMood = newMood => {
-
   return {
     type: RECEIVE_USER_MOOD,
     newMood
   }
 }
 
-export const createMood = (moodData) => (dispatch) =>  {
+const receiveMoodsExist = moodsExist => {
+  return {
+    type: RECEIVE_MOODS_EXIST,
+    moodsExist
+  }
+}
+
+export const createMood = (moodData) => (dispatch) => {
   return APIUtil.createMood(moodData)
     .then((newMood) => dispatch(receiveUserMood(newMood)));
+}
+
+
+export const fetchMoodsExist = () => (dispatch) => {
+  return APIUtil.fetchMoodsExist()
+    .then((moodsExist) => dispatch(receiveMoodsExist()));
 }
