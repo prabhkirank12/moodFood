85eec303b83eaf6ff8c459a5bcd81cd785dd8b84
dane <danemauland@gmail.com>
2020-12-02 20:17:10 -0500

    Finishes basic logo design

--- frontend/src/components/logo/logo.js
+++ frontend/src/components/logo/logo.js
@@ -3,31 +3,8 @@ import "./logo.scss";
 
 export default props => (
     <div className="logo-container">
-        <span>
-            M
-        </span>
-        <span>
-            O
-        </span>
+        <span>moodfood</span>
         <div className="circle down"></div>
-        <span>
-            O
-        </span>
-        <span>
-            D
-        </span>
-        <span>
-            F
-        </span>
-        <span>
-            O
-        </span>
         <div className="circle up"></div>
-        <span>
-            O
-        </span>
-        <span>
-            D
-        </span>
     </div>
 )
