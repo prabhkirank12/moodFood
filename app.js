8d7ccc579360bdc2e0305fb8d557899cd44ffdf3
prabhkirank12 <prabhkirank2@gmail.com>
2020-12-04 00:24:05 -0500

    Deploying to Heroku

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const {moods} = require("./routes/api/moods");
 const thirdParty = require("./routes/api/third-party");
 const bodyParser = require('body-parser');
 const passport = require("passport");
+const path = require('path');
 
 mongoose
     .connect(db, { useNewUrlParser: true })
@@ -18,6 +19,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(passport.initialize());
 require("./config/passport")(passport);
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('frontend/build'));
+    app.get('/', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    })
+}
 app.use("/api/users", users);
 app.use("/api/moods", moods);
 app.use("/api/third-party", thirdParty);
--- config/keys 2.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    mongoURI: 'mongodb+srv://Admin:ls1VyXgqKvzdlHIW@mern.9ae0m.mongodb.net/moodFood?retryWrites=true&w=majority',
-    secretOrKey: "Sto1tgsfBveUG4W1u5tdVrSwUBrws2Af",
-}
