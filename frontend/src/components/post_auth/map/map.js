901ec8875b6569983c0ecfcd177ea815b277de1e
dane <danemauland@gmail.com>
2020-12-05 15:57:40 -0500

    Removes unused code for the map and map buffer components

--- frontend/src/components/post_auth/map/map.js
+++ frontend/src/components/post_auth/map/map.js
@@ -4,37 +4,6 @@ import {withRouter} from "react-router-dom";
 import {fetchMapKey} from "../../../util/map_api_util";
 import "./map.scss";
 
-// const mapStateToProps = () => {
-//     return ({
-//         restaurant: {
-//             place_id: "ChIJiTxP17pYwokR0Gj0Wrd5cOg",
-//             icon: "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/restaurant-71.png",
-//             photos: [{
-//                 height: 849,
-//                 html_attributions: ["<a href=\"https://maps.google.com/maps/contrib/104833404427227649838\">A Google User</a>"],
-//                 photo_reference: "ATtYBwIRBDP1kaiq2_5yVkvhfCDW8-6zrYTeaY9eWsIfRPJQHLFu5Oe-RTj_XGu218ON4FNZloL6AjFNAqBmTPn06Gn06yTZCq_mCrBB12QgwTxh5lgmS5nqvDeLZL4CHYB7ItO840ltlDhACTmOJFaPj6nzbuLySOeeIuyHY5XzZU11Fn14",
-//                 width: 1600,
-//             }],
-//             price_level: 2,
-//             rating: 4.5,
-//             location: {
-//                 lat: 40.7794927,
-//                 lng: -73.9501884,
-//             },
-//             viewport: {
-//                 northeast: {
-//                     lat: 40.78090372989273,
-//                     lng: -73.94898387010727,
-//                 },
-//                 southwest: {
-//                     lat: 40.77820407010729,
-//                     lng: -73.95168352989272,
-//                 }
-//             }
-//         }
-//     })
-// }
-
 const mapStateToProps = (state, ownProps) => {
     console.log(ownProps)
     return {
@@ -42,12 +11,6 @@ const mapStateToProps = (state, ownProps) => {
         currentMood: ownProps.match.params.mood
     }
 }
-
-// const mapDispatchToProps = () => {
-//     return {
-//         fetchMapKey: () => dispatchEvent(fetchMapKey())
-//     }
-// }
 
 class Map extends React.Component {
 
